b630d72089f116e0cea331c92980ebca1daeabb5
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-03-23 15:34:52 +0100

    SD-624: add backend call

--- src/app/qr-code/scan-qr-code-button/scan-qr-code-button.component.ts
+++ src/app/qr-code/scan-qr-code-button/scan-qr-code-button.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-scan-qr-code-button',
@@ -8,7 +9,11 @@ import {Component} from '@angular/core';
 export class ScanQrCodeButtonComponent {
 
   state: ScanningState = "WAITING"
-  content?: string
+  content?: VerifySignedMessageResponse
+  tokenBalanceResponse?: FetchErc20TokenBalanceResponse
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   scanQrCode() {
     this.state = "SCANNING"
@@ -16,8 +21,31 @@ export class ScanQrCodeButtonComponent {
 
   handleScannedCode(text: string) {
     this.state = "CODE_SCANNED"
-    this.content = text
+    this.content = JSON.parse(JSON.parse(text)) as VerifySignedMessageResponse
+
+    const chainId = 80001
+    const messageId = this.content.id
+    const contractAddress = "0xe6b8a5cf854791412c1f6efc7caf629f5df1c747"
+
+    this.httpClient.get<FetchErc20TokenBalanceResponse>(
+      `http://localhost:8080/info/${chainId}/${messageId}/erc20-balance/${contractAddress}`,
+      {responseType: 'json'}
+    ).subscribe((value) => {
+      this.tokenBalanceResponse = value
+    })
   }
 }
 
 export type ScanningState = "WAITING" | "SCANNING" | "CODE_SCANNED"
+
+interface VerifySignedMessageResponse {
+  id: string
+  signature: string
+  valid_until: string
+}
+
+interface FetchErc20TokenBalanceResponse {
+  wallet_address: string
+  token_balance: string
+  token_address: string
+}
